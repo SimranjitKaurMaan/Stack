c6d11e6e81e77a65ef542c4f66dc2e8a4f2f90b5
Simranjit Kaur <simranjitkaur94mec@gmail.com>
2019-09-09 11:10:27 -0400

    Added stack

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Stack(5);
+            s.Push(10);
+            s.Push(20);
+            s.Push(30);
+            Console.WriteLine(s.Pop());
+
+            Console.WriteLine(s.Pop());
+            s.Clear();
+            Console.WriteLine(s.Pop());
+            
+
+        }
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    class Stack
+    {
+        object [] arr;
+        int nextIndex;
+
+        public Stack(int size)
+        {
+            arr = new object[size];
+            nextIndex = 0;
+        }
+
+        public void Push(object a)
+        {
+            if(a==null)
+            {
+                throw new InvalidOperationException();
+            }
+            arr[nextIndex] = a;
+            nextIndex++;
+        }
+
+        public object Pop()
+        {
+            if(nextIndex==0)
+            {
+                throw new InvalidOperationException();
+            }
+            object top = arr[nextIndex - 1];
+            nextIndex--;
+            return top;
+        }
+
+        public void Clear()
+        {
+            nextIndex = 0;
+        }
+
+    }
+}
